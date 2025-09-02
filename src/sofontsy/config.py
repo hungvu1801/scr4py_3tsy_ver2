991d61034ffeecec40c365724ed0874323261ec7
NguyenThuyComputer <vutienhung2212@gmail.com>
2025-09-02 14:31:52 +0700

    [fix] : separate data directory of CF and sofonsy. Remove unessessary lib in controller.py. [feat] : add Sofonsy project

---
+++ src/sofontsy/config.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://sofontsy.com"
+SUB_URL = "https://sofontsy.praella.com/products"
+# UPLOAD_SUFFIX = "/dashboard/product-add/graphic/"
+# SUCCESS_URL = "https://www.creativefabrica.com/dashboard/product-add/graphic/?thankyou=1"
+TAG_LIMIT = 22
