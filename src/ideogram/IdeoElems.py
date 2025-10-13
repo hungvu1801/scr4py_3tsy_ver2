44492e17bef9137d289c6cb9184b08f6b762b338
Thuy1 <thuymail@example.com>
2025-10-13 14:39:07 +0700

    [fix] - ideograme changed UI elems

--- src/ideogram/IdeoElems.py
+++ src/ideogram/IdeoElems.py
@@ -7,13 +7,14 @@ class IdeoElems:
     generating_notifier: str = "//p[@class='MuiTypography-root MuiTypography-body1 css-1ce06iw']"
     img_elems: str = "//div[contains(@class, 'MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-1')]/div"
 
-    ratio_elem: str = "//div[@class='MuiBox-root css-7hax1h']/div[2]"
-    ratio_input_width = "//div[@class='MuiBox-root css-1px0l42']/div[1]//input"
-    ratio_input_heigth = "//div[@class='MuiBox-root css-1px0l42']/div[2]//input"
+    ratio_elem: str = "//div[@class='MuiBox-root css-ritwfp']/div[2]"
+    ratio_elem_width_clickable: str = "(//p[@class='MuiTypography-root MuiTypography-body1 css-1t1m8m6'])[1]"
+    ratio_input_width = "(//div[@class=' MuiBox-root css-ndvclx']/div[1]/input)[1]"
+    ratio_input_heigth = "(//div[@class=' MuiBox-root css-ndvclx']/div[1]/input)[2]"
     ratio_save_btn: str = "//div[contains(text(), 'Save')]"
 
-    image_num_elem: str = "//div[@class='MuiBox-root css-7hax1h']/div[1]"
-    image_num_elem_btn: str = "//div[@class='MuiToggleButtonGroup-root css-1s9svmu']/button[{image_num}]"
+    image_num_elem: str = "//div[@class='MuiBox-root css-ritwfp']/div[1]"
+    image_num_elem_btn: str = "//div[@class='MuiToggleButtonGroup-root css-1mfgcko']/button[{image_num}]"
 
-    design_elem: str = "//div[@class='MuiBox-root css-7hax1h']/div[3]"
-    design_elem_btn: str = "//div[@class='MuiBox-root css-r7ft4d']/div[{design}]"
+    design_elem: str = "//div[@class='MuiBox-root css-ritwfp']/div[3]"
+    design_elem_btn: str = "//div[@class='MuiBox-root css-nyw7za']/div[{design}]"
