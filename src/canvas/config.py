82965b624c49a585d8b7d6fc08c6706d58c6fa5d
hungvt <vutienhung2212@gmail.com>
2025-10-20 14:27:50 +0700

    [feat]: add canva

---
+++ src/canvas/config.py
@@ -0,0 +1,3 @@
+MAIN_URL = "https://www.canva.com/"
+
+PROJECT_URL = "https://www.canva.com/folder/FAF2BPSq72A"
---
+++ src/canvas/elems.py
@@ -0,0 +1,7 @@
+class CanvasItems:
+    ...
+
+class CanvasElems:
+    PROJECT_BTN: str = "//div[@id='projects']"
+    PROJECT_FOLDER: str = "//div[@aria-label='new']"
+    IMG_CONTAINER: str = "//div[@class='SwlpcA']/div"
