bad93f4f18556e26ef4b9bdb7b8640f487d08a0b
hungvt <vutienhung2212@gmail.com>
2025-10-26 23:51:04 +0700

    [feat](canvas): add Canvas Elems

--- src/canvas/elems.py
+++ src/canvas/elems.py
@@ -3,5 +3,11 @@ class CanvasItems:
 
 class CanvasElems:
     PROJECT_BTN: str = "//div[@id='projects']"
-    PROJECT_FOLDER: str = "//div[@aria-label='new']"
+    # PROJECT_FOLDER: str = "//div[@aria-label='new']"
     IMG_CONTAINER: str = "//div[@class='SwlpcA']/div"
+
+    IMG_ELEM: str = "(//div[@class='SwlpcA']/div)[{number}]"
+    IMG_NAME: str = ".//span[@class='kb_Bkw giqgPw lA2MVA']"
+    IMG_MENU: str = ".//span[@class='vxQy1w']"
+
+    EDIT_IMG_BTN: str = "//button[@aria-label='Edit image']"
