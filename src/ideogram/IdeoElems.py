3c30eeb1c72d202ffa22e78d6ea23bc32cd95d97
Thuy1 <thuymail@example.com>
2026-02-28 13:19:49 +0700

    [fix]: UI elem of prompt box

--- src/ideogram/IdeoElems.py
+++ src/ideogram/IdeoElems.py
@@ -1,5 +1,5 @@
 class IdeoElems:
-    main_text_box: str = "//p[@data-placeholder='Generate new or upload & edit...']"
+    main_text_box: str = "//div[@class='MuiBox-root css-1hgt9n3']//p"
     # generate_button: str = "//button/p[contains(text(), 'Generate')]"
     generate_button: str = "//button[@class='nova-pill-button nova-pill-button-round']"
     policy_elem: str = "//p[contains(text(),'meet our content policy')]"
